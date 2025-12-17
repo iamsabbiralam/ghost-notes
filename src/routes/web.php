669cfca082bc3b1ea4a21231d160608cb62ed344
iamsabbiralam <sabbiriph4@gmail.com>
2025-12-18 00:03:54 +0600

    [ghost-notes] - upgrade code
    1. upgrade code

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,14 +4,19 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\File;
 
 Route::get('ghost-notes', function () {
-      $jsonPath = storage_path('app/ghost-notes.json');
+      $jsonPath = storage_path('app/ghost-notes/data.json');
 
-      if (app()->environment('production')) abort(403);
-
-      if (!File::exists($jsonPath)) {
-            return "Ghost Notes data not found. Please run 'php artisan ghost:write' first.";
+      if (app()->environment('production')) {
+            abort(403, 'Unauthorized in production.');
       }
 
+      if (!File::exists($jsonPath)) {
+            return "Ghost Notes data not found. <br> 
+                1. Run: <b>php artisan ghost:write</b> <br> 
+                2. Check if this file exists: <b>" . $jsonPath . "</b>";
+      }
+
+      // JSON Data
       $rows = json_decode(File::get($jsonPath), true);
 
       return view('ghost-notes::dashboard', ['rows' => $rows]);
